da6c822b794c4970c203603d58cb89aae7bafb02
Simo Savonen <simo.savonen@gmail.com>
2022-03-06 22:04:39 +0200

    add calculateBmi

---
+++ calculateBmi.ts
@@ -0,0 +1,18 @@
+const calculateBmi = (height: number, weight: number): string => {
+  if(height <= 0 || weight <= 0) {
+    return "Error: Provided values need to be positive numbers!" 
+  }
+  const heightInMeters = height / 100
+  const bmi = weight / (heightInMeters * heightInMeters)
+
+  if(bmi <= 16.0) { return "Underweight (Severe thinness)"; }
+  if(bmi <= 16.9) { return "Underweight (Moderate thinness)"; }
+  if(bmi <= 18.4) { return "Underweight (Mild thinness)"; }
+  if(bmi <= 24.9) { return "Normal (healthy weight)"; }
+  if(bmi <= 29.9) { return "Overweight (Pre-obese)"; }
+  if(bmi <= 34.9) { return "Obese (Class I)"; }
+  if(bmi <= 39.9) { return "Obese (Class II)"; }
+  if(bmi >= 40.0) { return "Obese (Class III)"; }
+}
+
+console.log(calculateBmi(180, 74));
